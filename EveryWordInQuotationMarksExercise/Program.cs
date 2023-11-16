f56b66dd213a03ea4c53849c4567f03443265f29
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-16 19:38:01 +0100

    Add project files.

---
+++ EveryWordInQuotationMarksExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EveryWordInQuotationMarksExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            string[] wordArray = userInput.Split(" ");
+
+            foreach (string word in wordArray)
+            {
+                Console.Write($"\"{word}\" ");
+            }
+        }
+    }
+}
